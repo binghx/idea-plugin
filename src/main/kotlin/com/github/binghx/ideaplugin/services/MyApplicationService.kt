f2fd5bb4fd0c916f32574bbf0ede7672966c0640
GitHub Action <action@github.com>
2021-04-15 06:47:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/binghx/ideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/binghx/ideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.binghx.ideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.binghx.ideaplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/binghx/ideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/binghx/ideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.binghx.ideaplugin.services
 
+import com.github.binghx.ideaplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
